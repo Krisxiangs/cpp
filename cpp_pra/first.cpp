6345f61e17d55bcf8bd8810ed1c19d2fe6c88787
Krisxiangs <kx142521@gmail.com>
2021-04-25 17:32:15 +0800

    123

---
+++ cpp_pra/first.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "power(2)"<<endl;
+    return 0;
+}
+
+
---
+++ cpp_pra/second.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << power(2)<<end;
+    return 0
+}
+double power(double n){
+    return n
+}
